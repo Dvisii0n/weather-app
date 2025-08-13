cf132a0d598d7ec06bdc04336a5cf2e22a39d9b8
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2025-08-13 16:13:38 -0600

    - Added utility function to reformat date depending on unit system - Added icon property to object returned by getWeather to change bg depending on icon value - Cleaned up repetition and reformatted code

--- src/logic.js
+++ src/logic.js
@@ -1,21 +1,51 @@
 const API_KEY = "USPQGAPMKBYC9ZM3K8NVMNL9L";
 
-export default async function getWeather(location, unit) {
+function capitalize(string) {
+    const arr = string.split("");
+    arr[0] = arr[0].toUpperCase();
+    return arr.join("");
+}
+
+function reformatDate(format, date) {
+    const arr = date.split("-");
+    const day = arr[2];
+    const month = arr[1];
+    const year = arr[0];
+    return format === "metric"
+        ? `${day}/${month}/${year}`
+        : `${month}/${day}/${year}`;
+}
+
+export default async function getWeather(location, units) {
+    const isMetric = units === "metric";
+
     try {
         const response = await fetch(
-            `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location.toLowerCase()}?unitGroup=${unit}&key=${API_KEY}&contentType=json`
+            `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location.toLowerCase()}?unitGroup=${units}&key=${API_KEY}&contentType=json`
         );
         const data = await response.json();
         const todays = data.days[0];
 
-        const address = data.address;
-        const temperature = todays.temp;
+        const date = isMetric
+            ? reformatDate("metric", todays.datetime)
+            : reformatDate("us", todays.datetime);
+        const address = capitalize(data.address);
+        const temperature = isMetric
+            ? `${todays.temp} °C`
+            : `${todays.temp} °F`;
         const conditions = todays.conditions;
-        const feelsLike = todays.feelslike;
-        const windSpeed = todays.windspeed;
-        const humidity = todays.humidity;
+        const feelsLike = isMetric
+            ? `Feels like ${todays.feelslike} °C`
+            : `Feels like ${todays.feelslike} °F`;
+        const windSpeed = isMetric
+            ? `Windspeed: ${todays.windspeed} km/h`
+            : `Windspeed: ${todays.windspeed} mph`;
+        const humidity = `Humidity: ${todays.humidity}%`;
+        const icon = todays.icon;
 
         return {
+            icon,
+            date,
             address,
             temperature,
             conditions,
@@ -24,6 +54,6 @@ export default async function getWeather(location, unit) {
             humidity,
         };
     } catch (error) {
-        throw error;
+        alert(`ERROR: Response failed`);
     }
 }
