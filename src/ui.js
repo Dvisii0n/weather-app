6fbe174509c15bb50092ee5c9494ccfceb514f3f
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2025-08-13 16:17:54 -0600

    Finished UI logic, yeah

--- src/ui.js
+++ src/ui.js
@@ -1,13 +1,33 @@
 import getWeather from "./logic";
 
+const fakeDelay = 1000;
+
 class UIBuilder {
-    constructor() {}
+    constructor() {
+        this.body = document.querySelector("body");
+    }
+
+    createContainerWithChilds(className, childs) {
+        const container = document.createElement("div");
+        container.className = className;
+
+        Object.keys(childs).forEach((key) => {
+            const child = document.createElement("div");
+            child.className = key;
+            child.textContent = childs[key];
+            container.appendChild(child);
+        });
+
+        return container;
+    }
 }
 
-export default class App {
+export default class App extends UIBuilder {
     constructor() {
-        this.builder = new UIBuilder();
+        super();
         this.form = document.querySelector("form");
+        this.weatherContainerClass = "weather-container";
+        this.loadingImg = document.querySelector(".mike");
     }
 
     #getInputData() {
@@ -17,12 +37,49 @@ export default class App {
         return inputData;
     }
 
+    refresh() {
+        const container = document.querySelector(
+            `.${this.weatherContainerClass}`
+        );
+
+        if (container) {
+            this.body.removeChild(container);
+            this.loadingImg.classList.toggle("visible");
+        }
+    }
+
+    async addWeatherContainer(units) {
+        const location = this.#getInputData();
+        if (location) {
+            setTimeout(async () => {
+                const data = await getWeather(location, units);
+                if (data) {
+                    const weatherContainer = this.createContainerWithChilds(
+                        this.weatherContainerClass,
+                        data
+                    );
+                    weatherContainer.classList.add(data.icon);
+                    weatherContainer.querySelector(".icon").textContent = "";
+                    this.body.appendChild(weatherContainer);
+                    this.loadingImg.classList.toggle("visible");
+                } else {
+                    alert("That location doesn't exist dumbass");
+                }
+            }, fakeDelay);
+        } else {
+            alert("Please enter a valid location");
+        }
+    }
+
     async run() {
         this.form.addEventListener("submit", async (event) => {
             event.preventDefault();
-            const location = this.#getInputData();
-            const data = await getWeather(location, "metric");
-            console.log(data);
+            this.refresh();
+            const units = document.querySelector('input[type="checkbox"]')
+                .checked
+                ? "us"
+                : "metric";
+            this.addWeatherContainer(units);
         });
     }
 }
